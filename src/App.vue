58ef87d413d759905587f2020508c2038d4014cb
gudaoxuri <i@sunisle.org>
2024-06-21 15:48:21 +0800

    Init.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import UploaderComp from './components/Uploader.vue';
+import { onMounted } from 'vue'
+import UploaderComp from './components/Uploader.vue'
 
 onMounted(() => {
 })
@@ -9,20 +9,5 @@ onMounted(() => {
 <template>
   <div class="text-base text-base-content bg-base-100">
     <UploaderComp />
-
-    <div class="row">
-      <p>Requested intent:</p>
-      <p id="update-intent" />
-    </div>
-
-    <div class="row">
-      <p>initial intent:<br></p>
-      <p id="initial-intent" />
-    </div>
-
-    <div class="row">
-      <p>updated intent by event:<br></p>
-      <p id="event-intent" />
-    </div>
   </div>
 </template>
